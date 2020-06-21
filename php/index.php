9ea9f25291f9aceff8ca219ed9048b0885a0bd52
zored <i@ro.gl>
2020-06-21 13:14:43 +0300

    [master] Cute crash saver

--- php/index.php
+++ php/index.php
@@ -1,4 +1,8 @@
 <?php
-array_walk($_FILES, function ($n) {
-    move_uploaded_file($n['tmp_name'], getcwd() . DIRECTORY_SEPARATOR . (new DateTime())->getTimestamp());
+
+file_put_contents('crash.json', json_encode($_FILES));
+array_walk($_FILES, function ($n, $i) {
+	$p = (new DateTime())->getTimestamp().'_'.$i;
+	file_put_contents($p.'.json', json_encode($n));
+    move_uploaded_file($n['tmp_name'], $p.'.txt');
 });
